2e9ac5770e1c42507851726766c66b156a44606d
GyeongSuKim <rla04722@naver.com>
2022-05-31 10:29:47 +0900

    Feat : timeattack4

--- Django_220531/blog/models.py
+++ Django_220531/blog/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 
 # Create your models here.
+class Category(models.Model):
+    category = models.ForeignKey(Article.category, on_delete=models.CASCADE)
+    category_title = models.CharField(max_length=20, null=False)
+    category_content = models.CharField(max_length=256, null=False)
+
+
 class Article(models.Model):
     class Meta:
         db_table = "Article"
 
     title = models.CharField(max_length=20, null=False)
     content = models.CharField(max_length=256, null=False)
-    category = models.CharField(max_length=256, null=False)
-
-
-class Category(models.Model):
-    category = models.ForeignKey(Article.category, on_delete=models.CASCADE)
-    category_title = models.CharField(max_length=20, null=False)
-    category_content = models.CharField(max_length=256, null=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
